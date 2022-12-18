d26d764716a0383ae7b33d479c6a32c205f04722
Paul <paul.cowan@cutting.scot>
2022-12-18 14:42:39 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import schema from "./form/schema.json";
+import { withTheme } from "@rjsf/core";
+import { Theme as MuiTheme } from "@rjsf/material-ui";
+import validator from "@rjsf/validator-ajv8";
+
+const Form = withTheme(MuiTheme);
+
+export const App = () => {
+  return (
+    <div className="container-fluid">
+      <Form
+        validator={validator}
+        schema={schema}
+        showErrorList={false}
+        noHtml5Validate
+      />
+    </div>
+  );
+};
